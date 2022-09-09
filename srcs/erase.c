55a7e9d7844640a5f83d91d6954880a07364b8bd
Illarion <pro100illarion@gmail.com>
2022-09-09 23:21:50 +0300

    Added element destruction

--- srcs/erase.c
+++ srcs/erase.c
@@ -6,7 +6,7 @@
 /*   By: illarion <glashli@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 14:38:41 by illarion          #+#    #+#             */
-/*   Updated: 2022/08/16 14:47:12 by illarion         ###   ########.fr       */
+/*   Updated: 2022/09/09 23:10:06 by illarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,9 @@ void	ft_erase(t_vector	*vector, size_t index)
 {
 	if (vector == NULL || index >= vector->count_elements)
 		return ;
+	if (vector->destructor)
+		(*vector->destructor)((t_uchar *)vector->storage
+			+ (index * vector->size_element));
 	if (ft_vector_memmove((t_uchar *)vector->storage
 			+ (index * vector->size_element),
 			(t_uchar *)vector->storage + ((index + 1)
@@ -35,10 +38,12 @@ void	ft_erase_front(t_vector	*vector)
 	ft_erase(vector, 0);
 }
 
+//		! Deprecated function !
 void	ft_erase_all(t_vector	*vector)
 {
 	if (vector == NULL)
 		return ;
+	ft_foreach_vector(vector, vector->destructor);
 	ft_vector_smart_free((void **)&vector->storage);
 	vector->count_elements = 0;
 	vector->capacity = 0;
